5cba73473daaa191bd815c4fd0279793b95afa95
insertjuninho <juniorsax3@gmail.com>
2018-10-22 09:56:24 -0300

    Atualizacao

--- sintomas.php
+++ sintomas.php
@@ -5,8 +5,24 @@
  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
  <meta http-eqiv="X-UA-Compatible" content="IE=edge" />
  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
- <link rel="stylesheet" type="text/css" href="css/sint.css">
- <title>SISTEMA</title>
+ <link rel="stylesheet" type="text/css" href="sint.css">
+ <style>
+ #f2,#f3,#f4{
+  display: none;
+}
+</style>
+<!-- <script>
+  function proxima(x){
+    document.getElementById("f1").style.display = "none";
+    document.getElementById("f2").style.display = "none";
+    document.getElementById("f3").style.display = "none";
+    //document.getElementById("f4").style.display = "none";
+    console.log(x);
+    document.getElementById("f"+x).style.display = "block";  
+  }
+
+</script> -->
+<title>SISTEMA</title>
 </head>
 <body>
   <header>
@@ -19,349 +35,57 @@
      <div class="col-sm-12 col-md-12" id="cad">
        <form id="msform" method="post" action="cad.php">
 
-
-        <!-- INICIAR O WHILE DO PHP AQUI -->
         <?php
         include "banco.php";
-          $pesquisar = "select * from sintoma";
-          $buscar = mysqli_query($con,$pesquisar);
-        while($b = mysqli_fetch_assoc($buscar)) {
-          $sintomas = $b['sintomas']; 
-         ?>
-        <fieldset>
-         <h2 class="fs-title"><?php echo "PAGINA 1 - 7"; ?></h2>
-         <div class="row">
-           <div class="col-md-6">
-            <label class="radio">
-              <div class="categoria">
-                <img src="img/ft.jpg" class="img-fluid" >
-                <input type="radio" name="page1" value="sintoma1" class="radio">
-                <p><?php echo $sintomas; ?></p><!-- buscar nome do banco -->
-              </div>
-            </label>
-          </div>
-        
-          <!-- FECHAR PHP -->
+        $sql="SELECT sintomas FROM categorias WHERE categorias = 'pe';";
+        $resultado=mysqli_query($con,$sql);
+        $y=0;
+        while ($row=mysqli_fetch_array($resultado,MYSQLI_ASSOC)){
+          $k[$y]=$row['sintomas'];
+          $y++;
+        }
+        $i=0;
+        $f=1;
+        $r=mysqli_num_rows($resultado); 
+        echo "<fieldset id='f{$f}'>";
 
-          <div class="col-md-6">
-            <label class="radio">
-              <div class="categoria">
-                <img src="img/ft.jpg" class="img-fluid" >
-                <input type="radio" name="page1" value="sintoma2" class="radio">
-                <p><?php echo $sintomas; ?></p><!-- buscar nome do banco -->
-              </div>
-            </label>
-          </div>
+        for($x=0; $x<$r;$x++){
+          if($i<=3){
+            $i++;
+            ?>  
 
-          <div class="col-md-6">
-            <label class="radio">
-              <div class="categoria">
-                <img src="img/ft.jpg" class="img-fluid" >
-                <input type="radio" name="page1" value="sintoma3" class="radio">
-                <p><?php echo $sintomas; ?></p><!-- buscar nome do banco -->
-              </div>
-            </label>
-          </div>
-
-          <div class="col-md-6">
-            <label class="radio">
-              <div class="categoria">
-                <img src="img/ft.jpg" class="img-fluid" >
-                <input type="radio" name="page1" value="sintoma4" class="radio">
-                <p><?php echo $sintomas; ?></p><!-- buscar nome do banco -->
-              </div>
-            </label>
-          </div>
-        </div>
-        <input type="button" name="next" class="next action-button" value="Próximo" />
-    </fieldset>
-           
-      <fieldset>
-       <h2 class="fs-title"><?php echo "PAGINA 2 - 7"; ?></h2>
-       <div class="row">
-         <div class="col-md-6">
-          <label class="radio">
-            <div class="categoria">
-              <img src="img/ft.jpg" class="img-fluid" >
-              <input type="radio" name="page2" value="sintoma5" class="radio">
-              <p></p><!-- buscar nome do banco -->
+            <div class="col-md-6">
+              <label class="radio">
+                <div class="categoria">
+                  <img src="img/ft.jpg" class="img-fluid" >
+                  <input type="radio" name="page7" value="sintoma25" class="radio">
+                  <p><?php echo $k[$x]; ?></p>
+                </div>
+              </label>
             </div>
-          </label>
-        </div>
-        <!-- FECHAR PHP -->
 
-        <div class="col-md-6">
-          <label class="radio">
-            <div class="categoria">
-              <img src="img/ft.jpg" class="img-fluid" >
-              <input type="radio" name="page2" value="sintoma6" class="radio">
-              <p></p><!-- buscar nome do banco -->
-            </div>
-          </label>
-        </div>
+            <?php
 
-        <div class="col-md-6">
-          <label class="radio">
-            <div class="categoria">
-              <img src="img/ft.jpg" class="img-fluid" >
-              <input type="radio" name="page2" value="sintoma7" class="radio">
-              <p></p><!-- buscar nome do banco -->
-            </div>
-          </label>
-        </div>
+          }else{
+            //  $f++;
+            echo "<input type='button' name='next' class='next action-button' value='Próximo'/></fieldset>";
+            $i=0;
+            $x=$x-1;
+            
+            ?>
+            <fieldset id="<?php echo $f."OK2" ?>">
+              <?php 
+            }
+          }
 
-        <div class="col-md-6">
-          <label class="radio">
-            <div class="categoria">
-              <img src="img/ft.jpg" class="img-fluid" >
-              <input type="radio" name="page2" value="sintoma8" class="radio">
-              <p></p><!-- buscar nome do banco -->
-            </div>
-          </label>
-        </div>
-      </div>
-      <input type="button" name="previous" class="previous action-button-previous" value="Voltar"/>
-      <input type="button" name="next" class="next action-button" value="Próximo"/>
-
-    </fieldset>
-
-    <fieldset>
-     <h2 class="fs-title"><?php echo "PAGINA 3 - 7"; ?></h2>
-     <div class="col-md-6">
-      <label class="radio">
-        <div class="categoria">
-          <img src="img/ft.jpg" class="img-fluid" >
-          <input type="radio" name="page3" value="sintoma9" class="radio">
-          <p></p><!-- buscar nome do banco -->
-        </div>
-      </label>
-    </div>
-    <!-- FECHAR PHP -->
-
-    <div class="col-md-6">
-      <label class="radio">
-        <div class="categoria">
-          <img src="img/ft.jpg" class="img-fluid" >
-          <input type="radio" name="page3" value="sintoma10" class="radio">
-          <p></p><!-- buscar nome do banco -->
-        </div>
-      </label>
-    </div>
-
-    <div class="col-md-6">
-      <label class="radio">
-        <div class="categoria">
-          <img src="img/ft.jpg" class="img-fluid" >
-          <input type="radio" name="page3" value="sintoma11" class="radio">
-          <p></p><!-- buscar nome do banco -->
-        </div>
-      </label>
-    </div>
-
-    <div class="col-md-6">
-      <label class="radio">
-        <div class="categoria">
-          <img src="img/ft.jpg" class="img-fluid" >
-          <input type="radio" name="page3" value="sintoma12" class="radio">
-          <p></p><!-- buscar nome do banco -->
-        </div>
-      </label>
-    </div>
-    <input type="button" name="previous" class="previous action-button-previous" value="Voltar"/>
-    <input type="button" name="next" class="next action-button" value="Próximo"/>
-  </fieldset>
-
-  <fieldset>
-   <h2 class="fs-title"><?php echo "PAGINA 4 - 7"; ?></h2>
-   <div class="col-md-6">
-    <label class="radio">
-      <div class="categoria">
-        <img src="img/ft.jpg" class="img-fluid" >
-        <input type="radio" name="page4" value="sintoma13" class="radio">
-        <p></p><!-- buscar nome do banco -->
-      </div>
-    </label>
-  </div>
-  <!-- FECHAR PHP -->
-
-  <div class="col-md-6">
-    <label class="radio">
-      <div class="categoria">
-        <img src="img/ft.jpg" class="img-fluid" >
-        <input type="radio" name="page4" value="sintoma14" class="radio">
-        <p></p><!-- buscar nome do banco -->
-      </div>
-    </label>
-  </div>
-
-  <div class="col-md-6">
-    <label class="radio">
-      <div class="categoria">
-        <img src="img/ft.jpg" class="img-fluid" >
-        <input type="radio" name="page4" value="sintoma15" class="radio">
-        <p></p><!-- buscar nome do banco -->
-      </div>
-    </label>
-  </div>
-
-  <div class="col-md-6">
-    <label class="radio">
-      <div class="categoria">
-        <img src="img/ft.jpg" class="img-fluid" >
-        <input type="radio" name="page4" value="sintoma16" class="radio">
-        <p></p><!-- buscar nome do banco -->
-      </div>
-    </label>
-  </div>
-  <input type="button" name="previous" class="previous action-button-previous" value="Voltar"/>
-  <input type="button" name="next" class="next action-button" value="Próximo"/>
-</fieldset>
-
-<fieldset>
- <h2 class="fs-title">PAGINA 5 - 7</h2>
- <div class="col-md-6">
-  <label class="radio">
-    <div class="categoria">
-      <img src="img/ft.jpg" class="img-fluid" >
-      <input type="radio" name="page5" value="sintoma17" class="radio">
-      <p></p><!-- buscar nome do banco -->
-    </div>
-  </label>
-</div>
-<!-- FECHAR PHP -->
-
-<div class="col-md-6">
-  <label class="radio">
-    <div class="categoria">
-      <img src="img/ft.jpg" class="img-fluid" >
-      <input type="radio" name="page5" value="sintoma18" class="radio">
-      <p></p><!-- buscar nome do banco -->
-    </div>
-  </label>
-</div>
-
-<div class="col-md-6">
-  <label class="radio">
-      <div class="categoria">
-        <img src="img/ft.jpg" class="img-fluid" >
-        <input type="radio" name="page5" value="sintoma19" class="radio">
-        <p></p><!-- buscar nome do banco -->
-      </div>
-    </label>
-</div>
-
-<div class="col-md-6">
-  <label class="radio">
-      <div class="categoria">
-        <img src="img/ft.jpg" class="img-fluid" >
-        <input type="radio" name="page5" value="sintoma20" class="radio">
-        <p></p><!-- buscar nome do banco -->
-      </div>
-    </label>
-</div>
-</ul>
-<input type="button" name="previous" class="previous action-button-previous" value="Voltar"/>
-<input type="button" name="next" class="next action-button" value="Próximo"/>
-</fieldset>
-
-<fieldset>
- <h2 class="fs-title"><?php echo "PAGINA 6 - 7"; ?></h2>
- <div class="col-md-6">
-  <label class="radio">
-    <div class="categoria">
-      <img src="img/ft.jpg" class="img-fluid" >
-      <input type="radio" name="page6" value="sintoma21" class="radio">
-      <p></p><!-- buscar nome do banco -->
-    </div>
-  </label>
-</div>
-<!-- FECHAR PHP -->
-
-<div class="col-md-6">
-  <label class="radio">
-    <div class="categoria">
-      <img src="img/ft.jpg" class="img-fluid" >
-      <input type="radio" name="page6" value="sintoma22" class="radio">
-      <p></p><!-- buscar nome do banco -->
-    </div>
-  </label>
-</div>
-
-<div class="col-md-6">
-  <label class="radio">
-      <div class="categoria">
-        <img src="img/ft.jpg" class="img-fluid" >
-        <input type="radio" name="page6" value="sintoma23" class="radio">
-        <p></p><!-- buscar nome do banco -->
-      </div>
-    </label>
-</div>
-
-<div class="col-md-6">
-  <label class="radio">
-      <div class="categoria">
-        <img src="img/ft.jpg" class="img-fluid" >
-        <input type="radio" name="page6" value="sintoma24" class="radio">
-        <p></p><!-- buscar nome do banco -->
-      </div>
-    </label>
-</div>
-<input type="button" name="previous" class="previous action-button-previous" value="Voltar"/>
-<input type="button" name="next" class="next action-button" value="Próximo"/>
-</fieldset>
-
-<fieldset>
- <h2 class="fs-title"><?php echo "PAGINA 7 - 7"; ?></h2>
- <div class="col-md-6">
-  <label class="radio">
-    <div class="categoria">
-      <img src="img/ft.jpg" class="img-fluid" >
-      <input type="radio" name="page7" value="sintoma25" class="radio">
-      <p></p><!-- buscar nome do banco -->
-    </div>
-  </label>
-</div>
-<!-- FECHAR PHP -->
-
-<div class="col-md-6">
-  <label class="radio">
-    <div class="categoria">
-      <img src="img/ft.jpg" class="img-fluid" >
-      <input type="radio" name="page7" value="sintoma26" class="radio">
-      <p></p><!-- buscar nome do banco -->
-    </div>
-  </label>
-</div>
-
-<div class="col-md-6">
-  <label class="radio">
-      <div class="categoria">
-        <img src="img/ft.jpg" class="img-fluid" >
-        <input type="radio" name="page7" value="sintoma27" class="radio">
-        <p></p><!-- buscar nome do banco -->
-      </div>
-    </label>
-</div>
-
-<div class="col-md-6">
-  <label class="radio">
-      <div class="categoria">
-        <img src="img/ft.jpg" class="img-fluid" >
-        <input type="radio" name="page7" value="sintoma28" class="radio">
-        <p></p><!-- buscar nome do banco -->
-      </div>
-    </label>
-</div>
-<input type="button" name="previous" class="previous action-button-previous" value="Voltar"/>
-<input type="submit" name="submit" class="cad" value="Cadastrar"/>
-</fieldset>
-<?php } ?>
+          ?>
+        </fieldset>
       </form>
     </div>
   </div>
-  </div>
+</div>
 <footer>
-  <p><?php echo "Desenvolvido por ©ACCIO - 2018"; ?></p> 
+  <p><?php echo "Desenvolvido por © ACCIO-2018"; ?></p>
 </footer>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
