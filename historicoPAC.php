28ce4425450b50396fd1f485b50bbd5ac646bf05
erickkf600 <35529628+erickkf600@users.noreply.github.com>
2018-10-30 11:55:30 -0300

    Add files via upload

---
+++ historicoPAC.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
+  <meta http-eqiv="X-UA-Compatible" content="IE=edge" />
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="css/histPac.css">
+
+  <title>SISTEMA</title>
+</head>
+<body>
+  <header>
+    <p class="logo">LOGO</p>
+    <a href="sair.php"><img src="img/out.png" class="out img-fluid"></a>
+  </header>
+  <div class="tabs">
+    <div class="row">
+        <ul class="nav nav-pills nav-stacked flex-column">
+          <li class="active"><a href="#tab_1" data-toggle="pill">Pág 1</a></li>
+          <li><a href="#tab_2" data-toggle="pill">Pág 2</a></li>
+          <li><a href="#tab_3" data-toggle="pill">Pág 3</a></li>
+          <li><a href="#tab_4" data-toggle="pill">Pág 4</a></li>
+          <li><a href="#tab_5" data-toggle="pill">Pág 5</a></li>
+          <li><a href="#tab_6" data-toggle="pill">Pág 6</a></li>
+          <li><a href="#tab_7" data-toggle="pill">Pág 7</a></li>
+        </ul>
+        <div class="col-md-10">
+          <div class="tab-content">
+            <div class="tab-pane active" id="tab_1">
+              <h3>First tab with soft transitioning effect.</h3>
+              <p>American Builders Inc. is your full service general contractor. We have been helping 
+                clients throughout Eastern North Carolina with their construction needs since 1996.
+                We take pride in understanding our clients' needs, making their construction experience 
+                as worry free as possible and only delivering a finished product that will withstand the
+              test of time. </p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod 
+              bibendum laoreet.</p>
+            </div>
+            <div class="tab-pane" id="tab_2">
+              <h3>Second tab with soft transitioning effect.</h3>
+              <p>We maintain a reputation for effective communication and collaboration between our 
+                team and clients to minimize surprises and ensure precise project delivery. Lorem ipsum 
+                dolor sit amet, consectetur adipiscing elit. Aenean euismod 
+                bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra 
+                justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque 
+              penatibus et magnis dis parturient montes.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod 
+              bibendum laoreet.</p>
+            </div>
+            <div class="tab-pane" id="tab_3">
+              <h3>Third tab with soft transitioning effect.</h3>
+              <p>Thank you for taking the time to consider American Builders Inc. as your general
+                contractor for any and all of your commercial, residential, insurance restoration and metal 
+                building needs. Please feel free to explore our website, and be sure to click on the
+              Facebook link at the bottom of this page and like us on Facebook.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod 
+              bibendum laoreet.</p>
+            </div>
+
+            <div class="tab-pane" id="tab_4">
+              <h3>Third tab with soft transitioning effect.</h3>
+              <p>Thank you for taking the time to consider American Builders Inc. as your general
+                contractor for any and all of your commercial, residential, insurance restoration and metal 
+                building needs. Please feel free to explore our website, and be sure to click on the
+              Facebook link at the bottom of this page and like us on Facebook.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod 
+              bibendum laoreet.</p>
+            </div>
+
+            <div class="tab-pane" id="tab_5">
+              <h3>Third tab with soft transitioning effect.</h3>
+              <p>Thank you for taking the time to consider American Builders Inc. as your general
+                contractor for any and all of your commercial, residential, insurance restoration and metal 
+                building needs. Please feel free to explore our website, and be sure to click on the
+              Facebook link at the bottom of this page and like us on Facebook.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod 
+              bibendum laoreet.</p>
+            </div>
+
+            <div class="tab-pane" id="tab_6">
+              <h3>Third tab with soft transitioning effect.</h3>
+              <p>Thank you for taking the time to consider American Builders Inc. as your general
+                contractor for any and all of your commercial, residential, insurance restoration and metal 
+                building needs. Please feel free to explore our website, and be sure to click on the
+              Facebook link at the bottom of this page and like us on Facebook.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod 
+              bibendum laoreet.</p>
+            </div>
+
+            <div class="tab-pane" id="tab_7">
+              <h3>Third tab with soft transitioning effect.</h3>
+              <p>Thank you for taking the time to consider American Builders Inc. as your general
+                contractor for any and all of your commercial, residential, insurance restoration and metal 
+                building needs. Please feel free to explore our website, and be sure to click on the
+              Facebook link at the bottom of this page and like us on Facebook.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod 
+              bibendum laoreet.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  <footer>
+    <p><?php echo "Desenvolvido por ©ACCIO - 2018"; ?></p> 
+  </footer>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="js/script.js"></script>
+</body>
+</html>
