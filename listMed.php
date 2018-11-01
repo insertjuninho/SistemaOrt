cdef9e84a06510131f4a1cf886b79c52f489c079
erickkf600 <erickkf600@gmail.com>
2018-11-01 11:58:36 -0300

    atualização

--- listMed.php
+++ listMed.php
@@ -16,7 +16,7 @@
   </header>
   <div class="container">
     <input class="pesq form-control form-control-lg" type="search" name="pesq" placeholder="Pesquisar Paciente" id="myInput">
-    <table class="table table-striped" id="func">
+    <table class="table" id="func">
       <thead>
         <tr>
           <th style="width:3%">ID</th>
@@ -27,11 +27,11 @@
       <?php 
       include "banco.php";
 
-      $sql    = ("SELECT * FROM paciente");
+      $sql    = ("SELECT * FROM acesso WHERE idMed >= 2");
       $result = mysqli_query($con , $sql);
       while ($row = mysqli_fetch_array($result)) {
-        $id   = $row['idPac'];
-        $nome = $row['nomePac'];
+        $id   = $row['idMed'];
+        $nome = $row['nomeMed'];
         ?>
         <tbody id="myTable">
           <tr>
@@ -40,27 +40,25 @@
             <td data-th="Nome" class="low"></td>
             <td id="name"><?php echo $nome; ?></td>
             <td data-th="Ação" class="low"></td>
-
             <td class="td-actions"><a href="ediPac.php"><button type="button" rel="tooltip" class="btn btn-info btn-just-icon btn-sm" data-original-title="" title="">
               <i class="material-icons">Detalhes</i>
             </button></a>
             <a href="historicoPAC.php"><button type="button" rel="tooltip" class="btn btn-success btn-just-icon btn-sm" data-original-title="" title="">
               <i class="material-icons">Histórico</i>
             </button></a>
+              <a href="delMed.php?idMed=<?php echo $id;?>">
             <button type="button" rel="tooltip" class="btn btn-danger btn-just-icon btn-sm" data-original-title="" title="">
-              <i class="material-icons">Deletar</i>
-            </button>
+                <i class="material-icons">Deletar</i>
+            </button></a>
           </td>          
         </tr>
       </tbody>
     <?php } ?>
   </table>
 </div>
-
 <footer>
   <p><?php echo "Desenvolvido por ©ACCIO - 2018"; ?></p> 
 </footer>
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
